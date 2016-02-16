44b9c514ac3b58cd2d9e8bfe085d709ba7e01fb6
Snorrib12 <Snorrus@gmail.com>
2016-02-16 17:50:45 +0000

    contrinuation from last push with folder changes, emptied out roomcontroller after the copy to newRoomController

--- src/rooms/RoomController.js
+++ src/rooms/RoomController.js
@@ -1,21 +1,2 @@
 chatApp.controller('roomController', ['$location', '$scope', 'socket', function($location ,$scope, socket){
-  $scope.newRoom={
-    name : '',
-    topic : '',
-    pass: ''
-  };
-  $scope.create = function(newRm){
-    var joinObj = {
-      var room = {
-        name :'',
-        topic
-      }
-    }
-    socket.emit('joinroom' , newRm ,function(available){
-      if(available){
-        //just a teporary redirect untill specific room pages are implemented
-        $location.url('/home/roomlist');
-      }
-    });
-  };
 }]);
