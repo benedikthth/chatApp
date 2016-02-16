fb30b30020cfefc40dc6354286094b0e7937742f
Snorrib12 <Snorrus@gmail.com>
2016-02-16 18:40:53 +0000

    made changes to the server so that it saves the topic correctly

--- server/chatserver.js
+++ server/chatserver.js
@@ -50,6 +50,7 @@ io.sockets.on('connection', function (socket) {
 			rooms[room] = new Room();
 			//Op the user if he creates the room.
 			rooms[room].ops[socket.username] = socket.username;
+			rooms[room].topic = joinObj.room.topic;
 			//If the user wants to password protect the room we set the password.
 			if(pass !== undefined) {
 				rooms[room].setPassword(pass);
