35a33750d2d77d4c0fb6464f39bd4f9559b0e7bf
Benedikt <benedikthth@gmail.com>
2016-02-16 16:43:26 +0000

    added some refs to socketresource

--- src/common/socketResource.js
+++ src/common/socketResource.js
@@ -1,4 +1,6 @@
 /* http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/ */
+/* http://briantford.com/blog/angular-socket-io */
+/* The following is appropriated from Brian Fords' github and blog. */ 
 chatApp.factory('socket',['$rootScope', function ($rootScope) {
 
   var socket = io.connect('ws://localhost:8080');
