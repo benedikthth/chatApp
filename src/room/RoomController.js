af5a09f02cb85f3344b21a7a7e39b4103fcff16f
benedikthth <benedikthth@gmail.com>
2016-02-17 03:12:21 +0000

    Did Work on roomController

--- src/room/RoomController.js
+++ src/room/RoomController.js
@@ -1,3 +1,39 @@
-chatApp.controller('roomController', ['$routeParams', '$location', '$scope', 'socket', function($routeParams, $location ,$scope, socket){
-  console.log($routeParams.id);
+chatApp.controller('roomController', ['user', '$routeParams', '$location', '$scope', 'socket', function(user, $routeParams, $location ,$scope, socket){
+  if(!user.isLogged){
+    $location.url('/login');
+  }
+  $scope.room = {};
+  socket.emit('joinroom', { room: $routeParams.id, pass:'' }, function(success, reason){
+    if(!success){
+      alert('unable to join room because : ' + reason);
+      $location.url('/roomlist');
+    } else {
+      console.log('you are connected to el '+ $routeParams.id);
+      $scope.roomName = $routeParams.id;
+      user.room = $routeParams.id;
+    }
+  });
+  socket.on('updateusers', function(roomName, userList, ops) {
+    if(roomName === $scope.roomName){
+      //message is meant for this rooms....?
+      $scope.room.userList = userList;
+      $scope.room.ops = ops;
+
+    }
+
+  });
+  socket.on('updatechat', function(room, messageHistory){
+    if($scope.roomName === room){
+      //message is not meant for this room.
+      $scope.messages = messageHistory;
+      return;
+    }
+  });
+  socket.on('updatetopic', function(room, topic, username){
+    if($scope.roomName === room){
+      //message is meant for this room.
+      $scope.topic = topic;
+      console.log(username);
+    }
+  });
 }]);
